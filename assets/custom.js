70106bd9915ea0080c70f1032315175a630ba88d
David <xxtrionxx@gmail.com>
2023-03-11 14:48:01 +0200

    Customize Input settings | Collection tabs ( with products )

---
+++ assets/custom.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $(document).ready(function () {       
+        let toggleTubs = (e) => {
+            e.preventDefault();
+            let parentItem = $(e.currentTarget).parent();
+            let elementActivatePosition = parentItem.index();
+
+            let tabHeadList = parentItem.parent().children();
+            let tabBodyList = parentItem.parent().parent().next().children().children();
+            let activateTab = (elements, activeIndex) => {
+                for (let item of elements) {
+                    $(item).removeClass('active');
+                    let elementPosition = $(item).index();
+                    if (activeIndex == elementPosition) {
+                        $(item).addClass('active');
+                    }				
+                }
+            }	
+            activateTab(tabHeadList, elementActivatePosition);
+            activateTab(tabBodyList, elementActivatePosition);	
+        }    
+        $(document).on('click', '.list__link', (e) => toggleTubs(e));       
+    });
+});
